410dbb5bfed0e5219928c05525b7dd3a52cd3713
Wormss9 <Wormss9.bd@gmail.com>
2021-04-16 16:11:32 +0200

    Added Player class deal and create_player and deal methods

--- game.py
+++ game.py
@@ -7,6 +7,19 @@ class Card:
         # __init__ je konstruktor => on vytvara instanciu objektu
         self.suit = suit
         self.number = number
+    def __str__(self):
+        return self.suit + " " + self.number
+
+class Player:
+    hand=[]
+    def __init__(self, number):
+        self.number=number
+    def __str__(self):
+        return "Player"+str(self.number)
+    def add_card(self,card:Card):
+        self.hand.append(card)
+    def sort(self):
+        pass
 
 
 class Deck:
@@ -16,9 +29,25 @@ class Deck:
     def fill_poker(self):
         suits = ['Spade', 'Diamond', 'Heart', 'Club']
         numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        for suit in suits:
-            for number in numbers:
-                self.deck.append(Card(suit, number))
+        if not self.deck:
+            for suit in suits:
+                for number in numbers:
+                    self.deck.append(Card(suit, number))
+            return True
+        return False
+
+    def fill_squad(self):
+        suits = ['Red', 'Yellow', 'Green']
+        numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+        if not self.deck:
+            for suit in suits:
+                for number in numbers:
+                    self.deck.append(Card(suit, number))
+                    self.deck.append(Card(suit, number))
+                self.deck.append(Card(suit, 'D'))
+            self.deck.append(Card('Rainbow', '1'))
+            return True
+        return False
 
     def add_card(self, card: Card):
         """Adds a card"""
@@ -35,3 +64,17 @@ class Deck:
     def pull_card(self):
         """Takes a card from the deck and returns it"""
         return self.deck.pop()
+
+def create_players(number:int):
+    players=[]
+    for x in range(number):
+        players.append(Player(x))
+        print("number "+str(x))
+    return players
+
+def deal(players:[Player],deck:Deck):
+    for x in range(int(len(deck.deck)/len(players))):
+        for player in players:
+            card=deck.pull_card()
+            print(str(player)+" got a "+str(card))
+            player.add_card(card)
