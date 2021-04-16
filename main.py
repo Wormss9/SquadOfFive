b00b1584591c355ac5aef1adaeee0efd526c3f7c
Wormss9 <Wormss9.bd@gmail.com>
2021-04-16 16:12:48 +0200

    Testing dealing cards

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from game import *
-from graphics import *
+# from graphics import *
 
-#to do => napisat hru
+# to do => napisat hru
 
+#Create and prepare deck
 deck = Deck()
-deck.fill_poker()
-deck.shuffle()
-for card in deck.deck:
-    print(card.number + " of " + card.suit + "s")
+deck.fill_squad()
+print(len(deck.deck))
+#deck.shuffle()
+
+#Create players
+players=create_players(4)
+#Deal cards
+test=[players[0],players[1]]
+deal(test, deck)
+
+
+print(len(players[0].hand))
+print(len(players[1].hand))
+print(len(players[2].hand))
+print(len(players[3].hand))
+
+print(str(players[0]))
+for card in players[0].hand:
+    print(str(card))
