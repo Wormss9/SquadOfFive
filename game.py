b52ac033681a0af2f1379bd757c38285ee418aff
Wormss9 <Wormss9.bd@gmail.com>
2021-04-16 17:11:09 +0200

    Making cards sortable

--- game.py
+++ game.py
@@ -3,21 +3,47 @@ import random
 
 class Card:
     """Card having a suit and a number."""
-    def __init__(self, suit, number):
-        # __init__ je konstruktor => on vytvara instanciu objektu
+    suitdict = {1: "Green", 2: "Yellow", 3: "Red", 4: "Rainbow"}
+
+    def __init__(self, suit: int, number: int):
+        # __init__ je konstruktor => on vytvara instanciu objektu premenna = Card()
         self.suit = suit
         self.number = number
+
     def __str__(self):
-        return self.suit + " " + self.number
+        # vracia objekt ako string = str(Card())
+        if self.number == 11:
+            return self.suitdict[int(self.suit)] + " Dragon"
+        return self.suitdict[self.suit] + " " + str(self.number)
+
+    def __eq__(self, other):
+        # vracia rovnost kariet card == othercard
+        if isinstance(other, Card):
+            return self.suit == other.suit and self.number == other.number
+        return NotImplemented
+
+    def __gt__(self, other):
+        # porovava velkost kariet podla cisel aj farby
+        if isinstance(other, Card):
+            if self.number == other.number and self.suit > other.suit:
+                return True
+            if self.number > other.number:
+                return True
+            return False
+        return NotImplemented
+
 
 class Player:
-    hand=[]
     def __init__(self, number):
-        self.number=number
+        self.number = number
+        self.hand = []
+
     def __str__(self):
-        return "Player"+str(self.number)
-    def add_card(self,card:Card):
+        return "Player" + str(self.number)
+
+    def add_card(self, card: Card):
         self.hand.append(card)
+
     def sort(self):
         pass
 
@@ -26,26 +52,16 @@ class Deck:
     """A deck capable of receiving shuffling and dealing cards."""
     deck = []
 
-    def fill_poker(self):
-        suits = ['Spade', 'Diamond', 'Heart', 'Club']
-        numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        if not self.deck:
-            for suit in suits:
-                for number in numbers:
-                    self.deck.append(Card(suit, number))
-            return True
-        return False
-
-    def fill_squad(self):
-        suits = ['Red', 'Yellow', 'Green']
-        numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    def fill(self):
+        suits = [1, 2, 3]
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         if not self.deck:
             for suit in suits:
                 for number in numbers:
                     self.deck.append(Card(suit, number))
                     self.deck.append(Card(suit, number))
-                self.deck.append(Card(suit, 'D'))
-            self.deck.append(Card('Rainbow', '1'))
+                self.deck.append(Card(suit, 11))
+            self.deck.append(Card(4, 1))
             return True
         return False
 
@@ -65,16 +81,15 @@ class Deck:
         """Takes a card from the deck and returns it"""
         return self.deck.pop()
 
-def create_players(number:int):
-    players=[]
+
+def create_players(number: int):
+    players = []
     for x in range(number):
         players.append(Player(x))
-        print("number "+str(x))
     return players
 
-def deal(players:[Player],deck:Deck):
-    for x in range(int(len(deck.deck)/len(players))):
+
+def deal(players: [Player], deck: Deck):
+    for x in range(int(len(deck.deck) / len(players))):
         for player in players:
-            card=deck.pull_card()
-            print(str(player)+" got a "+str(card))
-            player.add_card(card)
+            player.add_card(deck.pull_card())
--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 from game import *
+
 # from graphics import *
 
 # to do => napisat hru
 
-#Create and prepare deck
+# Create and prepare deck
 deck = Deck()
-deck.fill_squad()
-print(len(deck.deck))
-#deck.shuffle()
+deck.fill()
+deck.shuffle()
 
-#Create players
-players=create_players(4)
-#Deal cards
-test=[players[0],players[1]]
-deal(test, deck)
+# Create players
+players = create_players(4)
+# Deal cards
+deal(players, deck)
 
-
-print(len(players[0].hand))
-print(len(players[1].hand))
-print(len(players[2].hand))
-print(len(players[3].hand))
-
-print(str(players[0]))
+players[0].hand.sort()
 for card in players[0].hand:
     print(str(card))
