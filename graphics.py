3afed9df6be26fe4ff4c42df87e11abb16e7ff24
Wormss9 <Wormss9.bd@gmail.com>
2021-04-18 12:12:38 +0200

    Adding simple placeholder UI.

--- graphics.py
+++ graphics.py
@@ -1,9 +1,6 @@
 from tkinter import *
+from main import *
 
-master = Tk()
-
-master.geometry("500x500")
-master.title("Squad of Five")
 
 def open_login_window():
     # Toplevel object which will
@@ -21,11 +18,53 @@ def open_login_window():
     Label(newWindow,
           text="Select server or host").pack()
 
-btn = Button(master,
-             text="Play",
-             command=open_login_window)
-btn.pack(pady=10)
+
+master = Tk()
+master.geometry("1200x500")
+master.title("Squad of Five")
+master.configure(bg='grey')
+menu=Menu(master)
+master.config(menu=menu)
+subMenu=Menu(menu)
+menu.add_cascade(label="Game", menu=subMenu)
+subMenu.add_command(label="Connect", command=open_login_window)
+subMenu.add_command(label="Host")
 
 
+playerZone = Label(master, bg="darkGrey")
+cardZone= Label(master)
+deckZone = Label(master, bg="lightGrey")
+statusBar=Label(master, text="This is a status bar", relief='sunken')
+
+player1 = Label(playerZone, text=players[0].name)
+player2 = Label(playerZone, text=players[1].name)
+player3 = Label(playerZone, text=players[2].name)
+cardCount1 = Label(playerZone, text=len(players[0].hand))
+cardCount2 = Label(playerZone, text=len(players[1].hand))
+cardCount3 = Label(playerZone, text=len(players[2].hand))
+
+playerZone.grid()
+player1.grid(column=1, row=2, sticky=E)
+player2.grid(column=2, row=2)
+player3.grid(column=3, row=2, sticky=E)
+cardCount1.grid(column=1, row=3, sticky=E)
+cardCount2.grid(column=2, row=3)
+cardCount3.grid(column=3, row=3, sticky=E)
+
+cardZone.grid()
+
+deckZone.grid()
+cardsOnHand = []
+cardsOnHandChkBtn=[]
+for card in players[3].hand:
+    cardsOnHand.append(Checkbutton(deckZone, text=str(card)))
+x = 1
+for cardLabel in cardsOnHand:
+    cardLabel.grid(column=x,row=1)
+    x += 1
+deckZonePlay=Button(deckZone,text="   OK   ")
+deckZonePlay.grid(columnspan=len(players[3].hand))
+
+statusBar.grid(sticky='we')
 # mainloop, runs infinitely
 mainloop()
