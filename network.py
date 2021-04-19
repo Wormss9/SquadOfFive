57f56ab869131e98ccd3637feb31f554cc826a66
Wormss9 <Wormss9.bd@gmail.com>
2021-04-19 15:36:32 +0200

    Fixing dictonaries and refractoring inside.

--- network.py
+++ network.py
@@ -2,14 +2,14 @@ import socket
 import json
 from _thread import *
 
-server = "25.96.228.106"
-port = 5910
+defServer = "25.96.228.106"
+defPort = 5910
 
 
 class Server:
-    def __init__(self, game, server=server, port=port):
+    def __init__(self, game, server=defServer, port=defPort):
         self.conn = ""
-        self.addr = ""
+        self.address = ""
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.game = game
         try:
@@ -21,50 +21,50 @@ class Server:
 
     def threaded_client(self, conn):
         conn.send(str.encode("Connected"))
-        reply = ""
+        # reply = ""
         while True:
             try:
                 data = self.conn.recv(1024 * 2)
-                reply = json.dump(self.game.reply(json.loads(data)))
+                reply = self.game.reply(json.loads(json.loads(data)))
                 if not data:
-                    print(str(self.addr), " disconected.")
+                    print(str(self.address), " disconnected.")
                     break
                 else:
                     print("Received: ", reply)
-                self.conn.sendall(str.encode(reply))
-            except:
+                self.conn.sendall(str.encode(json.dumps(reply)))
+            except error:
+                print(str(error))
                 break
         print("Connection lost.")
         self.conn.close()
 
     def accept(self):
-        self.conn, self.addr = self.s.accept()
-        print("Connected to :", self.addr)
+        self.conn, self.address = self.s.accept()
+        print("Connected to :", self.address)
         start_new_thread(self.threaded_client, (self.conn,))
         # self.threaded_client()
 
 
 class Client:
-    def __init__(self, server=server, port=port):
+    def __init__(self, server=defServer, port=defPort):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.port = port
         self.server = server
-        self.addr = (self.server, self.port)
+        self.address = (self.server, self.port)
         self.pos = self.connect()
-
-    def getPos(self):
-        return self.pos
 
     def connect(self):
         try:
-            self.client.connect(self.addr)
+            self.client.connect(self.address)
             return self.client.recv(1024 * 2).decode()
         except error as e:
             str(e)
 
-    def send(self, data):
+    def send(self, data: dict):
+        if data is str:
+            data = {"info", data}
         try:
-            self.client.send(str.encode(data))
-            return self.client.recv(1024 * 2).decode()
+            self.client.send(str.encode(json.dumps(data)))
+            return self.client.recv(1024 * 2)
         except socket.error as e:
             print(e)
