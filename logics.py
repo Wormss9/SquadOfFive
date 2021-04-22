51a08ff1960c883f2c3511e81649f6035830e403
Wormss9 <Wormss9.bd@gmail.com>
2021-04-22 09:54:49 +0200

    Expanding game classes an client class.

--- logics.py
+++ logics.py
@@ -1,13 +1,12 @@
 import random
 import struct
-
 from network import *
 
 playPowerDic = {1: "single",
                 2: "pair",
                 3: "three of a kind",
                 4: "straight suite",
-                5: "flash",
+                5: "flush",
                 6: "full house",
                 7: "straight flush",
                 8: "gang of four",
@@ -19,7 +18,7 @@ playPowerDic = {1: "single",
 
 class Card:
     """Card having a suit and a number."""
-    suitdict = {1: "Green", 2: "Yellow", 3: "Red", 4: "Rainbow"}
+    suitDict = {1: "Red", 2: "Green", 3: "Blue", 4: "White"}
 
     def __init__(self, suit: int, number: int):
         # __init__ je konstruktor => on vytvara instanciu objektu premenna = Card()
@@ -27,10 +26,11 @@ class Card:
         self.number = number
 
     def __str__(self):
+        """Returns name of card"""
         # vracia objekt ako string = str(Card())
         if self.number == 11:
-            return self.suitdict[int(self.suit)] + " Dragon"
-        return self.suitdict[self.suit] + " " + str(self.number)
+            return self.suitDict[int(self.suit)] + " Diamond"
+        return self.suitDict[self.suit] + " " + str(self.number)
 
     def __eq__(self, other):
         # vracia rovnost kariet card == othercard
@@ -50,30 +50,42 @@ class Card:
 
 
 class Player:
-    def __init__(self, conn="local"):
-        self.name = ""
+    """Player as recognized by the server"""
+
+    def __init__(self):
+        self.name: str
         self.hand = []
-        self.conn = conn
+        self.connection: socket.socket
         self.connected = False
-        self.client = ""
+        self.client: str
 
     def __str__(self):
-        return "Player" + self.name
+        return self.name
 
     def connect(self, name: str):
         self.name = name
         self.connected = True
 
+    def disconnect(self):
+        # todo Actually call this function
+        self.connected = False
+
     def add_card(self, card: Card):
         self.hand.append(card)
 
-    def sort(self):
-        pass
+    def sort_cards(self):
+        self.hand.sort()
 
 
 class Deck:
-    """A deck capable of receiving shuffling and dealing cards."""
-    deck = []
+    """A random card deck."""
+
+    def __init__(self, players: [Player]):
+        self.players = players
+        self.deck = []
+        self.fill()
+        self.shuffle()
+        self.deal()
 
     def fill(self):
         suits = [1, 2, 3]
@@ -88,10 +100,6 @@ class Deck:
             return True
         return False
 
-    def add_card(self, card: Card):
-        """Adds a card"""
-        return self.deck.append(card)
-
     def shuffle(self):
         """Shuffles a non empty deck"""
         if (self.deck):
@@ -104,77 +112,116 @@ class Deck:
         """Takes a card from the deck and returns it"""
         return self.deck.pop()
 
+    def deal(self):
+        for x in range(int(len(self.deck) / len(self.players))):
+            for player in self.players:
+                player.add_card(self.pull_card())
+
 
 class Play:
+    """Defines the cards on the table and the ones you are putting on it."""
+
     def __init__(self, cards: [Card]):
         self.cards = cards
 
     def value(self):
-        # single
+        self.cards.sort()
+        flush = False
+        straight = False
+        """Returns the value of the hand. Usefull for comparison. Needed for first play."""
+        # finding out wether you have suit or a flush because they have no expressive value alone
+        if len(self.cards) == 5:
+            if self.cards[0].suit == self.cards[1].suit == self.cards[2].suit == self.cards[3].suit == self.cards[
+                4].suit:
+                flush = True
+            if self.cards[4].number == self.cards[3].number + 1 == self.cards[2].number + 2 == self.cards[
+                1].number + 3 == self.cards[0].number + 4:
+                straight = True
+        # 1: "single"
         if len(self.cards) == 1:
-            return [self.cards[0].number, 0, self.cards[0].color, 0, 1]
-        # pair
+            return 1
+        # 2: "pair"
         if len(self.cards) == 2 and self.cards[0].number == self.cards[1].number:
-            return [card.number, 0, 1]
-        #
-        return False
+            return 2
+        # 3: "three of a kind"
+        if len(self.cards) == 3 and self.cards[0].number == self.cards[1].number == self.cards[2].number:
+            return 3
+        # 4: "straight"
+        if straight and not flush:
+            return 4
+        # 5: "flush"
+        if flush and not straight:
+            return 5
+        # todo 6: "full house"
+
+        # 7: "straight flush"
+        if flush and straight:
+            return 7
+        # 8: "gang of four"
+        if len(self.cards) == 4:
+            pass
+        # 9: "gang of five"
+        if len(self.cards) == 5:
+            pass
+        # 10: "gang of six"
+        if len(self.cards) == 6:
+            pass
+        # 11: "gang of seven"
+        if len(self.cards) == 7:
+            pass
+        return 0
 
     def __gt__(self, other):
-        if power(self.cards) and power(other.cards):
-            if len(self.cards) == len(other.cards):
-                if len(self.cards) == 1:
-                    return self.cards[0] > other.cards[0]
+        my_case = puttable_on_empty_table(self)
+        other_case = puttable_on_empty_table(other)
+        if my_case == other_case == 1:
+            return self.cards[0] > other.cards[0]
+        if my_case == other_case == 2:
+            # not comparing all card by color
+            return self.cards[0] > other.cards[0]
+        if my_case == other_case == 3:
+            # not comparing all card by color
+            return self.cards[0] > other.cards[0]
+        # todo
         return False
 
 
 class GameClient:
-    def __init__(self):
+    def __init__(self, client_holder):
         self.settings = Settings()
-        self.ip = Settings().adress
-        self.name = Settings().name
+        self.ip = self.settings.adress
+        self.name = self.settings.name
         self.hand = []
-        self.chatTextArea = ""
-        self.connection = ""
+        self.connection: NetworkClient
+        self.client_holder=client_holder
 
-    def connect(self, ip, window):
+    def connect(self, ip):
         try:
-            self.connection = Client(server=ip)
-            self.connection.connect(self)
-            print(json.dumps({'name': self.name}))
-            self.connection.send({'name': self.name})
+            self.connection = NetworkClient(response_function=self.respond, server_ip=ip)
+            self.connection.connect()
+            self.connection.send(to_dict('name', self.name))
         except error as e:
-            print(e)
-            return "Connection failed"
-        window.destroy()
-        return "Connected"
+            print_type("GameClient.connect", e)
+            return (False, "Connection failed")
+        return (True, "Connected")
 
-    def send(self, message):
-        print("Sending: ", str(message))
+    def send(self, message: dict):
         self.connection.send(message)
 
-    def set_name(self, name, window):
+    def set_name(self, name):
         self.name = name
-        window.destroy()
-        return "Name changed to " + name
+        return (True, "Name changed to " + name)
 
-    def reply(self, data):
+    def respond(self, data):
         reply = {}
         for key in data:
-            value = self.answer(key, data.get(key), self.client)
-            #printt(value)
-            reply.update(value)
-        #print("Final reply ", str(reply))
+            reply.update(self.process_respondable(key, data.get(key)))
         return reply
 
-    def answer(self, key, word, client):
+    def process_respondable(self, key, word):
         print("Processing: ", key, str(word).replace('\n', ' '))
         if key == "chat":
-            #print("Chat got: ", word.replace('\n', ' '))
-            self.chatTextArea['text'] = word
-            return {}
-        elif key == 'connected':
-            if word:
-                self.client = client
+            self.client_holder.chatTextArea['text'] = word
             return {}
         else:
             return {}
@@ -263,20 +310,3 @@ class Settings:
                 "adress": self.adress}
         with open('settings.txt', 'w') as outfile:
             json.dump(data, outfile)
-
-
-def create_players(number: int):
-    players = []
-    for x in range(number):
-        players.append(Player(x))
-    return players
-
-
-def deal(players: [Player], deck: Deck):
-    for x in range(int(len(deck.deck) / len(players))):
-        for player in players:
-            player.add_card(deck.pull_card())
-
-
-def printt(x):
-    print(type(x), " ", str(x))
