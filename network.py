d804cd01252903a7d6924ee50d744db9f4a3731f
Wormss9 <Wormss9.bd@gmail.com>
2021-04-25 14:49:38 +0200

    Client capable of informing about disconnection

--- network.py
+++ network.py
@@ -149,4 +149,5 @@ class NetworkClient:
                     break
             except (socket.error, KeyboardInterrupt) as e:
                 print_type("NetworkClient.listen", e)
+                self.response_function(to_dict('reply', "Disconnected"))
                 break
