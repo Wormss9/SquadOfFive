a281f436ad14d51d5d9c396174ab9b53589e568e
Wormss9 <Wormss9.bd@gmail.com>
2022-12-30 15:01:52 +0100

    Remove shared library

--- shared/src/card/color.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Color {
-    Red = 0,
-    Green = 1,
-    Blue = 2,
-    White = 3,
-}
--- shared/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod card;
-mod hand;
-
-pub use card::Card;
-pub use hand::Hand;
