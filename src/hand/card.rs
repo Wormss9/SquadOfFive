9067ed3d47b63cc96fa3a37d449b159e354bdf17
Wormss9 <Wormss9.bd@gmail.com>
2022-12-30 15:33:28 +0100

    Add card

---
+++ src/hand/card.rs
@@ -0,0 +1,79 @@
+use std::cmp::Ordering;
+
+mod color;
+mod value;
+
+use color::Color;
+use value::Value;
+
+#[derive(Debug, Clone)]
+pub struct Card {
+    pub value: Value,
+    pub color: Color,
+}
+
+impl Card {
+    pub fn new(value: u8, color: u8) -> Self {
+        Self {
+            value: Value::from_u8(value),
+            color: Color::from_u8(color),
+        }
+    }
+    pub fn to_path(&self) -> String {
+        format!("{:}{:02}", self.color.to_char(), self.value.to_u8())
+    }
+}
+
+impl PartialEq for Card {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value && self.color == other.color
+    }
+}
+
+impl Eq for Card {}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let value_comparison = self.value.cmp(&other.value);
+        if value_comparison == Ordering::Equal {
+            self.color.cmp(&other.color)
+        } else {
+            value_comparison
+        }
+    }
+}
+
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn eq() {
+        assert_eq!(Card::new(1, 1), Card::new(1, 1));
+        assert_ne!(Card::new(1, 1), Card::new(2, 1));
+        assert_ne!(Card::new(1, 1), Card::new(1, 2));
+    }
+
+    #[test]
+    fn ord() {
+        assert!(Card::new(2, 2) > Card::new(1, 2));
+        assert!(Card::new(2, 2) > Card::new(2, 1));
+        assert!(Card::new(3, 2) > Card::new(3, 1));
+        assert!(Card::new(2, 1) > Card::new(1, 3));
+        assert!(Card::new(2, 2) > Card::new(1, 1));
+    }
+
+    #[test]
+    fn path() {
+        assert_eq!(Card::new(1, 1).to_path(), "r01".to_string());
+        assert_eq!(Card::new(5, 2).to_path(), "g05".to_string());
+        assert_eq!(Card::new(11, 3).to_path(), "b11".to_string());
+        assert_eq!(Card::new(1, 4).to_path(), "w01".to_string());
+    }
+
+}
