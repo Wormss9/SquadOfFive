68394652fb80b702afe7d8a1e47464f5d847cab2
Wormss9 <Wormss9.bd@gmail.com>
2023-04-01 21:21:42 +0200

    Use barchart on play end

--- frontend/src/components/PlayEnd.vue
+++ frontend/src/components/PlayEnd.vue
@@ -1,22 +1,55 @@
 <template>
   <div class="gameEnd">
-    <div class="players">
-      <div v-for="player in players" :key="player.id">
-        <p>{{ player.nick }}</p>
-        <img
-          v-bind:src="'data:image/png;base64,' + player.avatar"
-          class="avatar"
-        />
-      </div>
-    </div>
+    <BarChart class="endChart" v-bind="barChartProps" />
     <button @click="next" class="link-like-button button">Continue</button>
   </div>
 </template>
 
 <script lang="ts">
+import { computed } from "vue";
 import { defineComponent, PropType } from "vue";
+import { BarChart, useBarChart } from "vue-chart-3";
 import { Gamer } from "@/api/types";
+import { Chart, ChartData, ChartOptions, registerables } from "chart.js";
+
+Chart.register(...registerables);
+
+const toScore = (ammount: number) => {
+  if (ammount == 16) {
+    return 80;
+  }
+  if (ammount >= 14) {
+    return ammount * 4;
+  }
+  if (ammount >= 11) {
+    return ammount * 3;
+  }
+  if (ammount >= 8) {
+    return ammount * 2;
+  }
+  return ammount;
+};
+
+const toColor = (ammount: number) => {
+  if (ammount == 16) {
+    return "#000";
+  }
+  if (ammount >= 14) {
+    return "#00F";
+  }
+  if (ammount >= 11) {
+    return "#0F0";
+  }
+  if (ammount >= 8) {
+    return "#F00";
+  }
+  return "#FFF";
+};
+
 export default defineComponent({
+  components: {
+    BarChart,
+  },
   props: {
     players: {
       type: Array as PropType<
@@ -24,6 +57,38 @@ export default defineComponent({
       >,
       default: () => [],
     },
+  },
+  data() {
+    const testData = computed<ChartData<"bar">>(() => ({
+      labels: this.players.map((player) => player.nick),
+      datasets: [
+        {
+          data: this.players.map((player) => toScore(player.cards ?? 0)),
+          backgroundColor: this.players.map((player) =>
+            toColor(player.cards ?? 0)
+          ),
+        },
+      ],
+    }));
+    const options = computed<ChartOptions<"bar">>(() => ({
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          enabled: false,
+        },
+      },
+    }));
+    const { barChartProps, barChartRef } = useBarChart({
+      chartData: testData,
+      options,
+    });
+
+    return {
+      barChartProps,
+      barChartRef,
+    };
   },
   methods: {
     next() {
@@ -36,6 +101,9 @@ export default defineComponent({
 <style lang="scss">
 .gameEnd {
   position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   top: 10%;
   left: 10%;
   width: 80%;
@@ -45,10 +113,8 @@ export default defineComponent({
   z-index: 1000;
   background-color: #222;
 }
-
-.players {
-  display: flex;
-  justify-content: center;
-  text-align: center;
+.endChart {
+  width: 100%;
+  height: 80%;
 }
 </style>
